123525d6ab2a2243ec0164e7b6d358f030312a4b
arezit <32492661+arezit@users.noreply.github.com>
2017-10-23 17:59:13 -0200

    Delete conn.php

--- Includes/conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$host = "localhost";
-	$user = "root";
-	$pass = "";
-	$bank = "tex";
-	$conn = mysqli_connect($host, $user, $pass) or die("Impossível conectar-se ao servidor ".$host);
-	$link = mysqli_select_db($conn, $bank) or die ("Impossível conectar-se ao banco ".$bank); //Today is good day to die!!!!!! -- by: Lukinhas;
-?>
